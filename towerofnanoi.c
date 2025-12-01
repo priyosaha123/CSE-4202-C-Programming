55644e8b55d74225f987fc51ae35a12b1685cad5
priyosaha123 <shaha-25024022@aaub.edu.bd>
2025-12-01 21:15:04 +0600

    Added Tower of Hanoi

---
+++ towerofnanoi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+void tower(int n, char s, char h, char d)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    tower(n - 1, s, d, h);
+    printf("%c->%c\n", s, d);
+
+    tower(n - 1, h, s, d);
+    return;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the Number of disks: \n");
+    scanf("%d", &n);
+
+    tower(n, 'A', 'B', 'C');
+    return 0;
+}
